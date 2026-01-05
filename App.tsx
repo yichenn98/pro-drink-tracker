57edb14f4720a2be254ccd1063d021ad9fad4ec7
Iris <yichencheng105@gmail.com>
2026-01-06 00:02:18 +0800

    Add sugar/ice icons on record cards

--- App.tsx
+++ App.tsx
@@ -24,6 +24,41 @@ const Icons = {
 
 const SugarIcon = () => (
   <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="14"
+    height="14"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M12 2v20" />
+    <path d="M6 6l12 12" />
+    <path d="M18 6L6 18" />
+  </svg>
+);
+
+const IceIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="14"
+    height="14"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <polygon points="12 2 22 12 12 22 2 12 12 2" />
+  </svg>
+);
+
+
+const SugarIcon = () => (
+  <svg
     width="14"
     height="14"
     viewBox="0 0 24 24"
