8205804598064191e2d7fa5c5d7c3a9caf869af9
yichenn98 <59024224+yichenn98@users.noreply.github.com>
2026-01-06 16:25:26 +0800

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,5 +1,5 @@
 
 import { IceLevel, SweetnessLevel } from './types';
 
-export const ICE_LEVELS: IceLevel[] = ['固定', '微冰', '去冰', '溫熱'];
-export const SWEETNESS_LEVELS: SweetnessLevel[] = ['固定', '半糖', '三分糖', '二分糖', '一分糖', '無糖'];
+export const ICE_LEVELS: IceLevel[] = ['固定', '正常冰', '少冰', '微冰', '去冰', '溫熱'];
+export const SWEETNESS_LEVELS: SweetnessLevel[] = ['固定', '全糖', '少糖', '半糖', '微糖', '二分糖', '一分糖', '無糖'];
