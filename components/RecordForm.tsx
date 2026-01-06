b271215c0973a5578745da7c539db084fea58a7c
yichenn98 <59024224+yichenn98@users.noreply.github.com>
2026-01-06 22:46:42 +0800

    Update RecordForm.tsx

--- components/RecordForm.tsx
+++ components/RecordForm.tsx
@@ -237,35 +237,7 @@ const RecordForm: React.FC<RecordFormProps> = ({
         >
           Save
         </button>
-       <button
-  type="button"
-  onClick={onCancel}
-  aria-label="Close"
-  className="
-    absolute top-6 right-6
-    p-2 rounded-full
-    text-stone-300
-    hover:text-stone-600
-    hover:bg-stone-100
-    transition
-    active:scale-90
-  "
->
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="20"
-    height="20"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2.5"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <path d="M18 6 6 18" />
-    <path d="M6 6 18 18" />
-  </svg>
-</button>
+    
 
 
       </div>
